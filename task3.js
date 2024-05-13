2e332397748bbad564174e2c74a645cdbb60c38e
ArtemStart96 <156700753+ArtemStart96@users.noreply.github.com>
2024-05-13 23:13:59 +0300

    Create task3.js

---
+++ task3.js
@@ -0,0 +1,5 @@
+function changeBackground() {
+    var colors = ['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#ff00ff', '#00ffff'];
+    var randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.body.style.backgroundColor = randomColor;
+}
